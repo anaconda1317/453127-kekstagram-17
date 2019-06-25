57c285f1ff80a9e03715d41d6343a052fbdd5c02
anaconda1317 <kate.klimanovski@gmail.com>
2019-06-25 21:25:21 +0300

    effects level

--- js/main.js
+++ js/main.js
@@ -123,7 +123,7 @@ var inputEffectSepia = effectsListElement.querySelector('#effect-sepia');
 var inputEffectMarvin = effectsListElement.querySelector('#effect-marvin');
 var inputEffectPhobos = effectsListElement.querySelector('#effect-phobos');
 var inputEffectHeat = effectsListElement.querySelector('#effect-heat');
-
+var currentEffectName = effectsListElement.querySelector('.effects__radio:checked');
 var effectLevelScale = imgUploadOverlay.querySelector('.img-upload__effect-level');
 
 // Валидация комментариев, #тег
@@ -142,6 +142,7 @@ inputIdUploadFile.addEventListener('change', function () {
 var onPopupUploadEscPress = function (evt) {
   if (evt.keyCode === ESC_KEYCODE) {
     closePopup();
+    // очищаем форму после каждой загрузки изображения
     imgUploadForm.reset();
   }
 };
@@ -165,6 +166,13 @@ inputEffectNone.addEventListener('click', function () {
   uploadImagePreview.style.filter = '';
   effectLevelScale.classList.add('hidden');
 });
+// строгое сравнение, если checked  на input id="effect-none" стоит, то шкала скрыта
+if (currentEffectName === inputEffectNone) {
+  effectLevelScale.classList.add('hidden');
+} else {
+  effectLevelScale.classList.remove('hidden');
+}
+
 
 inputEffectChrome.addEventListener('click', function () {
   uploadImagePreview.style.filter = 'grayscale(1)';
@@ -173,29 +181,143 @@ inputEffectChrome.addEventListener('click', function () {
 
 inputEffectSepia.addEventListener('click', function () {
   uploadImagePreview.style.filter = 'sepia(1)';
+  effectLevelScale.classList.remove('hidden');
 });
 
 inputEffectMarvin.addEventListener('click', function () {
   uploadImagePreview.style.filter = 'invert(100%)';
+  effectLevelScale.classList.remove('hidden');
 });
 
 inputEffectPhobos.addEventListener('click', function () {
   uploadImagePreview.style.filter = 'blur(5px)';
+  effectLevelScale.classList.remove('hidden');
 });
 
 inputEffectHeat.addEventListener('click', function () {
+  // inputEffectHeat.checked = false;
   uploadImagePreview.style.filter = 'brightness(3)';
+  effectLevelScale.classList.remove('hidden');
 });
+
 
 // Валидация комментариев,если фокус находится в поле ввода комментария,
 //  нажатие на Esc не должно приводить к закрытию формы редактирования изображения
-// событие focus происходит, когда таб переходим или мышкой в фокусе те кода элемент становится активным
-// когда нажимаем esc происх соб keydoun
+// событие focus происходит, когда таб переходим или мышкой в фокусе те когда элемент становится активным
+// когда нажимаем esc происх событие keydoun
 
 
-focusTextarea .addEventListener('keydown', function (evt) {
+focusTextarea.addEventListener('keydown', function (evt) {
   // focusTextarea === evt.target;
   if (evt.keyCode === ESC_KEYCODE) {
     evt.stopPropagation();
   }
 });
+
+
+// Цикл Drag-and-drop для маркера
+var effectLevelValue = document.querySelector('.effect-level__value');
+// возможность перемещать пин слайдера
+var effectLevelPin = document.querySelector('.effect-level__pin');
+// Глубина эффекта фотографии yellow - ее длинна шкалы регулируется положением Pin
+var effectLevelDepth = document.querySelector('.effect-level__depth');
+// Глубина эффекта фотографии - вся шкала - yellow +opacity
+var effectLevelLine = document.querySelector('.effect-level__line');
+
+
+// функция перевода количества пикселей в % - ширину линии effectLevelLine.offsetWidth делим на 100% - получаем значение 1% в px,
+// Pin.offset.left делим на значение 1% в px -находим колич в % Pin.offset.left
+var pixelToPersent = function (offsetLeft) {
+  return offsetLeft / (effectLevelLine.offsetWidth / 100);
+};
+
+
+// подгатавливаемся к перемещению drag and drop
+effectLevelPin.addEventListener('mousedown', function (evt) {
+  // evt.preventDefault();
+
+  // начальные координаты мыши, когда мы нажмем мышью пин - mousedown
+  var startCoordPin = {
+    x: evt.clientX,
+    y: evt.clientY
+  };
+  // console.log(startCoordPin);
+
+  // дали другое название для события moveEvt - иначе перетрет evt
+  var onMouseMovePin = function (moveEvt) {
+    // moveEvt.preventDefault();
+    // shift это смещение ( 10-начало +5 =15; 5 это смещение, а 15 новое значение для начальной точки)
+    var shiftPin = {
+      x: startCoordPin.x - moveEvt.clientX
+    };
+    // мы перезаписали новое значение для начальной точки (10+5 =15, было 10, а стало 15)
+    startCoordPin = {
+      x: moveEvt.clientX
+    };
+    var pinPositionValue = pixelToPersent(effectLevelPin.offsetLeft - shiftPin.x);
+
+    if (pinPositionValue <= 0) {
+      pinPositionValue = 0;
+    } else if (pinPositionValue >= 100) {
+      pinPositionValue = 100;
+    }
+
+    effectLevelPin.style.left = pinPositionValue + '%';
+    effectLevelDepth.style.width = pinPositionValue + '%';
+    effectLevelValue.value = pinPositionValue;
+    // вызываем функцию changeFilterLevel, которя изменяет уровень насыщенности фильтра в зависимости от позиции pin
+    // pinPositionValue -ее аргумент
+    changeFilterLevel(pinPositionValue);
+  };
+
+  // дали другое название для события upEvt иначе перетрет evt
+  var onMouseUpPin = function (upEvt) {
+    onMouseMovePin(upEvt);
+
+    // очищаем обработчики пч мы еще работаем в интерфейсе и совершаем маусапы на странице постоянно
+    document.removeEventListener('mousemove', onMouseMovePin);
+    document.removeEventListener('mouseup', onMouseUpPin);
+  };
+
+  document.addEventListener('mousemove', onMouseMovePin);
+  document.addEventListener('mouseup', onMouseUpPin);
+});
+
+
+//  у uploadImagePreview изменяем уровень насыщенности фильтра в зависимости от позиции пина
+
+var changeFilterLevel = function (pinPositionValue) {
+
+  var pinPosition = Math.round(pinPositionValue);
+
+  effectLevelValue.value = pinPosition;
+
+  // var currentChecked определена внутри функции changeFilterLevel, при каждом запуске этой функции вызывается запрос и он нам
+  // возвращает текущий элемент .effects__radio:checked, когда двигаем пин у нас всегда запускается функция сhangeFilterLevel
+  // а если бы var currentChecked была снаружи функции, то она бы нашлась один раз по ходу загрузки скрипта в соответсвии с разметкой кода
+
+  var currentChecked = effectsListElement.querySelector('.effects__radio:checked');
+  var value = currentChecked.value;
+
+  switch (value) {
+    case 'chrome':
+      uploadImagePreview.style.filter = 'grayscale(' + pinPosition * 0.01 + ')';
+      break;
+    case 'sepia':
+      uploadImagePreview.style.filter = 'sepia(' + pinPosition * 0.01 + ')';
+      break;
+    case 'marvin':
+      uploadImagePreview.style.filter = 'invert(' + pinPosition + '%)';
+      break;
+    case 'phobos':
+      uploadImagePreview.style.filter = 'blur(' + pinPosition * 0.05 + 'px)';
+      break;
+    case 'heat':
+      uploadImagePreview.style.filter = 'brightness(' + pinPosition * 0.03 + ')';
+      break;
+    default:
+      uploadImagePreview.style.filter = '';
+  }
+};
+
+
