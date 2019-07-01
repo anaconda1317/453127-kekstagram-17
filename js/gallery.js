a5b9967c0bb5097e086a5a057c6baa392bf0d0c9
anaconda1317 <kate.klimanovski@gmail.com>
2019-07-01 21:04:00 +0300

    xhr request

--- js/gallery.js
+++ js/gallery.js
@@ -2,40 +2,7 @@
 (function () {
 
   // Генерация комментария (аватар, имя, сообщение)
-  var getRandomComment = function () {
-    var message = window.util.getRandomElement(window.data.MESSAGES);
-    var message1 = window.util.getRandomElement(window.data.MESSAGES);
-    var comment = {
-      avatar: 'img/avatar-' + window.util.getRandomNumber(1, window.data.MAX_URL_AVATAR) + '.svg',
-      message: message + message1,
-      name: window.util.getRandomElement(window.data.NAMES)
-    };
-
-    return comment;
-  };
-
-  var getComments = function () {
-    var countLength = window.util.getRandomNumber(1, 6);
-    var comments = [];
-    for (var i = 0; i < countLength; i++) {
-      comments[i] = getRandomComment();
-    }
-    return comments;
-  };
-
-  var getRandomPhotos = function () {
-    var photos = [];
-    for (var i = window.data.MIN_URL; i <= window.data.MAX_URL; i++) {
-      var photo = {
-        url: 'photos/' + i + '.jpg',
-        likes: window.util.getRandomNumber(window.data.LIKE.MIN, window.data.LIKE.MAX),
-        comments: getComments()
-      };
-      // Добавляет объект в массив
-      photos.push(photo);
-    }
-    return photos;
-  };
+  /* все что между звездочками - комментарий */
 
   var pictureTemplate = document.querySelector('#picture').
     content.
@@ -53,14 +20,17 @@
     return photoElement;
   };
 
-  // упаковываем созданные с помощью renderPhoto объекты в фрагмент
-  var photos = getRandomPhotos();
-  var fragment = document.createDocumentFragment();
-  // Мы сложили в массив фотки, а цикл организован по массиву - и мы опираеcя на порядок элементов в массиве (он начся с 0)
-  for (var i = 0; i < photos.length; i++) {
-    fragment.appendChild(renderPhoto(photos[i]));
-  }
+  // упаковываем полученные с сервера по GET запросу массив объектов в фрагмент(импортируем функцию window.load)
+  window.load(function (photos) {
+    var fragment = document.createDocumentFragment();
+    // Мы сложили в массив фотки, а цикл организован по массиву - и мы опираеcя на порядок элементов в массиве (он начся с 0)
+    for (var i = 0; i < photos.length; i++) {
+      fragment.appendChild(renderPhoto(photos[i]));
+    }
 
-  picturesBlock.appendChild(fragment);
+    picturesBlock.appendChild(fragment);
+
+  });
+
 
 })();
